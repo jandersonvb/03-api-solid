99426ab9198b5aadb366318e3e92274c52e41737
jandersonvb <jandersonvb.dev@gmail.com>
2024-05-26 16:42:17 -0300

    feat: create validatation from .env

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { app } from "./app";
+import { env } from "./env";
 
-app.listen({ port: 3333, host: '0.0.0.0' })
+app.listen({ port: env.PORT, host: '0.0.0.0' })
   .then(() => console.log("🚀 Server is running on port 3333"))
