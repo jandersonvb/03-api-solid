c4d5ab6c0d6deb3cba9724d50488fa4d1ec8b392
jandersonvb <jandersonvb.dev@gmail.com>
2024-05-27 14:36:28 -0300

    feat: configure ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({ port: env.PORT, host: '0.0.0.0' })
-  .then(() => console.log("🚀 Server is running on port 3333"))
+app
+  .listen({ port: env.PORT, host: '0.0.0.0' })
+  .then(() => console.log('🚀 Server is running on port 3333'))
